53f691b04039bf313760c5a6afa378c9f6878486
Weicheng Wang <109393929+weicheng998@users.noreply.github.com>
2026-06-22 16:54:18 -0500

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        # Pair each car's position, speed, and time to finish
+        cars = [(p, s, (target - p) / s) for p, s in zip(position, speed)]
+        # Sort according to the position
+        cars.sort()
+        # Use a monotonic stack to keep track of fleets
+        leader = cars.pop()
+        n_fleet = 1
+        while cars:
+            if cars[-1][2] <= leader[2]:
+                # The top of the stack will catch up the current leader
+                cars.pop()
+            else:
+                leader = cars.pop()
+                n_fleet += 1
+        return n_fleet

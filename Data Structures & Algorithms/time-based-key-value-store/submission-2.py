1cac2bb16cbb5620ad435d7e49736bc4371e2aeb
Weicheng Wang <109393929+weicheng998@users.noreply.github.com>
2026-06-28 15:09:35 -0500

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+class TimeMap:
+    def __init__(self):
+        self.m = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.m[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        v_list = self.m[key]
+        if not v_list:
+            return ""
+        # Binary search through v for the largest timestamp_prev
+        # v[i][0] is the prev timestamp and is strictly ascending
+        l, r = 0, len(v_list) - 1
+        max_t_p, max_t_p_v = v_list[l][0], v_list[l][1]
+        while l <= r:
+            mid = (l + r) // 2
+            t, v = v_list[mid]
+            if t == timestamp:
+                return v
+            elif t > timestamp:
+                r = mid - 1
+            else:  # t < timestamp
+                if t > max_t_p:
+                    max_t_p = t
+                    max_t_p_v = v
+                l = mid + 1
+        if max_t_p < timestamp:
+            return max_t_p_v
+        else:
+            return ""

087e5a30d2cdc5fc54de477fa7b72a9e34312b56
Weicheng Wang <109393929+weicheng998@users.noreply.github.com>
2026-09-10 16:14:28 -0500

    Add: implement-prefix-tree - submission-2

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-2.py
@@ -0,0 +1,36 @@
+class TrieNode:
+    def __init__(self, is_end=False, children=None) -> None:
+        self.is_end: bool = is_end
+        self.children: dict = children if children is not None else {}
+
+
+class PrefixTree:
+    def __init__(self):
+        self.root: TrieNode = TrieNode()
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+        for char in word:
+            if char in cur.children:
+                cur = cur.children[char]
+            else:
+                node = TrieNode()
+                cur.children[char] = node
+                cur = node
+        cur.is_end = True
+
+    def search(self, word: str) -> bool:
+        cur = self._traverse(word)
+        return cur is not None and cur.is_end
+
+    def startsWith(self, prefix: str) -> bool:
+        return self._traverse(prefix) is not None
+
+    def _traverse(self, word: str) -> Optional[TrieNode]:
+        cur = self.root
+        for char in word:
+            if char in cur.children:
+                cur = cur.children[char]
+            else:
+                return None
+        return cur

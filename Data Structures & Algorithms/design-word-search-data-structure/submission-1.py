7cf02e3795941305d5b5946585f815ca4e20a064
Weicheng Wang <109393929+weicheng998@users.noreply.github.com>
2026-09-11 15:24:18 -0500

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.py
@@ -0,0 +1,50 @@
+from collections import deque
+
+
+class TrieNode:
+    def __init__(self, is_end=False, children=None) -> None:
+        self.is_end: bool = is_end
+        self.children: dict = children if children is not None else {}
+
+
+class WordDictionary:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for char in word:
+            if char in cur.children:
+                cur = cur.children[char]
+            else:
+                node = TrieNode()
+                cur.children[char] = node
+                cur = node
+        cur.is_end = True
+
+    def search(self, word: str) -> bool:
+        def udpateCandidates(candidates: deque, target: str) -> bool:
+            size = len(candidates)
+            if size == 0:
+                return False
+            found = False
+            for _ in range(size):
+                node = candidates.popleft()
+                if target == ".":
+                    found = True
+                    for child in node.children.values():
+                        candidates.append(child)
+                else:  # target != '.'
+                    if char in node.children:
+                        candidates.append(node.children[char])
+                        found = True
+            return found
+
+        candidates = deque([self.root])
+        for char in word:
+            if not udpateCandidates(candidates, char):
+                return False
+        for candidate in candidates:
+            if candidate.is_end:
+                return True
+        return False
